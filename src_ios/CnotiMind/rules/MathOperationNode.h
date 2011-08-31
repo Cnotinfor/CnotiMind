ada9139dc99bb5d378485f2ad99332a65c2c9d27
Gonçalo Rodrigues <goncalo.rodrigues@cnotinfor.pt>
2011-08-31 17:25:25 +0100

    Added class MathOperationNode

---
+++ src_ios/CnotiMind/rules/MathOperationNode.h
@@ -0,0 +1,13 @@
+//
+//  MathOperationNode.h
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 8/31/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "RuleNode.h"
+
+@interface MathOperationNode : RuleNode
+
+@end
---
+++ src_ios/CnotiMind/rules/MathOperationNode.m
@@ -0,0 +1,23 @@
+//
+//  MathOperationNode.m
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 8/31/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "MathOperationNode.h"
+
+@implementation MathOperationNode
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
