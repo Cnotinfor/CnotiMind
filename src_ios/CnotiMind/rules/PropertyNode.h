dae9b3c65e84d4f32aaa14ff22ffa5bc230bdf5d
Gonçalo Rodrigues <goncalo.rodrigues@cnotinfor.pt>
2011-08-31 18:40:14 +0100

    Added class PropertyNode.

---
+++ src_ios/CnotiMind/rules/PropertyNode.h
@@ -0,0 +1,13 @@
+//
+//  PropertyNode.h
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 8/31/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "RuleNode.h"
+
+@interface PropertyNode : RuleNode
+
+@end
