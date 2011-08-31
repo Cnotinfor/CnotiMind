4746bbccac46a0d18577f15d25d968e8289e5396
Gonçalo Rodrigues <goncalo.rodrigues@cnotinfor.pt>
2011-08-31 17:05:57 +0100

    Added class DeleteNode and implemented deleteEvent method on Brain class.

---
+++ src_ios/CnotiMind/rules/DeleteNode.h
@@ -0,0 +1,13 @@
+//
+//  DeleteNode.h
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 8/31/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "RuleNode.h"
+
+@interface DeleteNode : RuleNode
+
+@end
---
+++ src_ios/CnotiMind/rules/DeleteNode.m
@@ -0,0 +1,23 @@
+//
+//  DeleteNode.m
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 8/31/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "DeleteNode.h"
+
+@implementation DeleteNode
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
