3b10a9552cda035f3ace1a6a89516be5c7db928c
Gonçalo Rodrigues <goncalo.rodrigues@cnotinfor.pt>
2011-09-01 17:12:58 +0100

    Completed class SettingsXmlHandler , but not tested it.

--- src_ios/CnotiMind/SettingsXmlHandler.m
+++ src_ios/CnotiMind/SettingsXmlHandler.m
@@ -163,40 +163,158 @@
 
 - (BOOL) elementPerception:(GDataXMLElement*)atts
 {
-
-    return false;    
+    // Test if it inside another element
+    if( _insidePerception || _insideAction || _insideEmotion )
+    {
+        return false;
+    }
+    // Check it it need values for the perception
+    _needValues = ![[atts valueForKey:TYPE] caseInsensitiveCompare:TYPE_VALUES];
+    [_possibleValues removeAllObjects];
+    
+    // Get the perception name
+    _elementName = [atts valueForKey:NAME];
+                    
+    if([_elementName length]==0)
+    {
+        return false;
+    }
+    
+    _insidePerception = true;
+    
+    return true;  
 }
+
 
 
 - (BOOL) elementAction:(GDataXMLElement*)atts
 {
-
-    return false;    
+    // Test if it inside another element
+    if( _insidePerception || _insideAction || _insideEmotion )
+    {
+        return false;
+    }
+    // Check it it need values for the perception
+    _needValues = ![[atts valueForKey:TYPE] caseInsensitiveCompare:TYPE_VALUES];
+    [_possibleValues removeAllObjects];
+    
+    // Get the action name
+    _elementName = [atts valueForKey:NAME];
+    
+    if([_elementName length]==0)
+    {
+        return false;
+    }
+    
+    _insideAction = true;
+    
+    return true;    
 }
 
 - (BOOL) elementEmotion:(GDataXMLElement*)atts
 {
 
-    return false;
+    // Test if it inside another element
+    if( _insidePerception || _insideAction || _insideEmotion )
+    {
+        return false;
+    }
+    
+    // Get the emotion name
+    _elementName = [atts valueForKey:NAME];
+
+    if([_elementName length]==0)
+    {
+        return false;
+    }
+    
+    // Test if it has all the attributes required for the emotions
+    if( [atts attributeForName:@"value"] != nil &&
+       [atts attributeForName:@"max"] != nil &&
+       [atts attributeForName:@"min"] != nil )
+    {
+        bool ok;
+        int i;
+        [_emotionAttributes removeAllObjects];
+        
+        
+        if ([CnotiMind isNumeric: [[atts attributeForName:@"value"] stringValue]]) {
+            i = [[[atts attributeForName:@"value"] stringValue] intValue];
+        }
+        else {
+            DLog("[SettingsXmlHandler::elementEmotion] Invalid attribute value: value = %d", i);
+            return false;
+        }
+        [_emotionAttributes addObject:[NSNumber numberWithInt:i]];
+        
+        
+        if ([CnotiMind isNumeric: [[atts attributeForName:@"max"] stringValue]]) {
+            i = [[[atts attributeForName:@"max"] stringValue] intValue];
+        }
+        else {
+            DLog("[SettingsXmlHandler::elementEmotion] Invalid attribute value: max = %d", i);
+            return false;
+        }
+        [_emotionAttributes addObject:[NSNumber numberWithInt:i]];
+        
+        
+        if ([CnotiMind isNumeric: [[atts attributeForName:@"min"] stringValue]]) {
+            i = [[[atts attributeForName:@"min"] stringValue] intValue];
+        }
+        else {
+            DLog("[SettingsXmlHandler::elementEmotion] Invalid attribute value: min = %d", i);
+            return false;
+        }
+        [_emotionAttributes addObject:[NSNumber numberWithInt:i]];
+    }
+    else
+    {
+        return false;
+    }
+    
+    _insideEmotion = true;
+    return true;
+
 }
+
+
+
 
 - (BOOL) endElementPerception
 {
-
+    if( _insidePerception )
+    {
+        [_brain addValidPerception:_elementName];
+        _insidePerception = false;
+        return true;
+    }
     return false;
 }
 
 
 - (BOOL) endElementAction
 {
-
-    return false;    
+    if( _insideAction )
+    {
+        [_brain addValidAction:_elementName];
+        _insideAction = false;
+        return true;
+    }
+    return false; 
 }
 
 
 - (BOOL) endElementEmotion
 {
 
+    if( _insideEmotion )
+    {
+        Emotion* e = [[Emotion alloc] initWithNameAndValueAndMaxAndMin:_elementName value:[[_emotionAttributes objectAtIndex:0] floatValue] max:[[_emotionAttributes objectAtIndex:1] floatValue] min:[[_emotionAttributes objectAtIndex:2] floatValue]];
+        [_brain addEmotion:e];
+        _insideEmotion = false;
+        return true;
+    }
+//    TODO
     return false;
 }
 
