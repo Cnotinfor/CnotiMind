deb8a9e03a6af82b6b2914e9961aad7ec3e789e5
Gonçalo Rodrigues <goncalo.rodrigues@cnotinfor.pt>
2011-09-01 14:15:19 +0100

    Added RandomNode class.

---
+++ src_ios/CnotiMind/rules/RandomNode.h
@@ -0,0 +1,13 @@
+//
+//  RandomNode.h
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 9/1/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "RuleNode.h"
+
+@interface RandomNode : RuleNode
+
+@end
---
+++ src_ios/CnotiMind/rules/RandomNode.m
@@ -0,0 +1,23 @@
+//
+//  RandomNode.m
+//  SingleEmotion
+//
+//  Created by Gonçalo Rodrigues on 9/1/11.
+//  Copyright 2011 Universidade de Coimbra. All rights reserved.
+//
+
+#import "RandomNode.h"
+
+@implementation RandomNode
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
